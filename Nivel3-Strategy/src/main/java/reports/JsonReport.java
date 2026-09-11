d63bb6abf18da2a695e3bfef17a3931986d68838
Enzo979899 <enzogallardosimon@gmail.com>
2026-09-11 22:21:03 +0200

    feat: add json report strategy

---
+++ Nivel3-Strategy/src/main/java/reports/JsonReport.java
@@ -0,0 +1,18 @@
+package reports;
+
+import service.Report;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonReport implements Report {
+    @Override
+    public void generateReport(String content, String fileName) {
+
+        try (FileWriter writer = new FileWriter(fileName + ".json")) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
