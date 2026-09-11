00f6e8814aeb4c3bf90f96008fa9f15eba8f87a8
Enzo979899 <enzogallardosimon@gmail.com>
2026-09-11 20:17:26 +0200

    feat: add SpainContactFactory class

---
+++ Nivel2-Factory/src/main/java/factory/SpainContactFactory.java
@@ -0,0 +1,19 @@
+package factory;
+
+import formats.AddressData;
+import formats.Spain.SpainAddress;
+import formats.Spain.SpainPhone;
+import interfaces.Address;
+import interfaces.Phone;
+
+public class SpainContactFactory implements ContactFactory {
+    @Override
+    public Address createAdress(AddressData data) {
+        return new SpainAddress(data);
+    }
+
+    @Override
+    public Phone createPhone(String number) {
+        return new SpainPhone(number);
+    }
+}
