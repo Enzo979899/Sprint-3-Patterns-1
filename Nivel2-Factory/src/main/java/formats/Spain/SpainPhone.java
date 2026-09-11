78263d81dc8456695688a056c4c0f3e44ddd534c
Enzo979899 <enzogallardosimon@gmail.com>
2026-09-11 20:15:57 +0200

    feat: add SpainPhone implementation

---
+++ Nivel2-Factory/src/main/java/formats/Spain/SpainPhone.java
@@ -0,0 +1,17 @@
+package formats.Spain;
+
+import interfaces.Phone;
+
+public class SpainPhone implements Phone {
+    private static final String PREFIX = "+34 ";
+    private final String numero;
+
+    public SpainPhone(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return PREFIX + numero;
+    }
+}
